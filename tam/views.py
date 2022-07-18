0232d2506561239972a35f473a4c04b48be28367
benie <davinci.monalissa@gmail.com>
2022-07-18 20:30:55 +0300

    create a url pattern for home page & create views & update template

--- tam/views.py
+++ tam/views.py
@@ -8,3 +8,9 @@ def profile(request,user_id):
     user = request.user
     message = 'Welcome, ' + user
     return render('user/profile.html',{"profile":profile,"message":message})
+
+def home(request,user_id):
+    profile = User.objects.all().filter(pk=user_id)
+    user = request.user
+    message = 'Welcome, ' + user
+    return render('content/index.html',{"profile":profile,"message":message})
