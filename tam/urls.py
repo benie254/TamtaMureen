198d0727c84a5b835af28d4228b3e9e5eb54c8bc
benie <davinci.monalissa@gmail.com>
2022-07-18 09:26:34 +0300

    fix error in url paths

--- tam/urls.py
+++ tam/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('admin', admin.site.urls,name='admin'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('django_registration.backends.one_step.urls')),
-    path('accounts/register', auth_views.LoginView.as_view(template_name='django_registration/registration_form.html'),name='signup'),
+    
     path('accounts/profile', auth_views.LoginView.as_view(template_name='registration/login.html'),name='signin'),
     path('accounts/signout', auth_views.LogoutView.as_view(template_name='registration/login.html'),name='signout'),
     # path('',views.home,name='home'),
