f8cd4bff3e3166e010b4238e31eeedef3c2bde43
benie <davinci.monalissa@gmail.com>
2022-07-18 09:05:53 +0300

    remove unused import

--- tam/urls.py
+++ tam/urls.py
@@ -1,14 +1,13 @@
 from django.urls import path,re_path as url
 from . import views
 from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls,name='admin'),
-    path('accounts/login/', auth_views.LoginView.as_view(template_name='registration/login.html'),name='login'),
-    path('accounts/logout/', auth_views.LogoutView.as_view(template_name='registration/login.html'),name='logout'),
+    path('admin', admin.site.urls,name='admin'),
+    path('accounts/signin', auth_views.LoginView.as_view(template_name='registration/login.html'),name='signin'),
+    path('accounts/signout', auth_views.LogoutView.as_view(template_name='registration/login.html'),name='signout'),
     path('',views.home,name='home'),
 ]
