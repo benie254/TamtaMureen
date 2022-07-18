12b3143455e4b74601a53e9387595e9c4058b1a7
benie <davinci.monalissa@gmail.com>
2022-07-18 09:17:15 +0300

    update urls

--- tam/urls.py
+++ tam/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path,re_path as url
+from django.urls import path,re_path as url,include
 from . import views
 from django.conf import settings
 from django.contrib import admin
@@ -7,7 +7,9 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin', admin.site.urls,name='admin'),
-    path('accounts/signin', auth_views.LoginView.as_view(template_name='registration/login.html'),name='signin'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('django_registration.backends.one_step.urls')),
+    path('accounts/profile', auth_views.LoginView.as_view(template_name='registration/login.html'),name='signin'),
     path('accounts/signout', auth_views.LogoutView.as_view(template_name='registration/login.html'),name='signout'),
     # path('',views.home,name='home'),
 ]
