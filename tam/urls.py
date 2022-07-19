d5202e1d98407ef58ebaa8d9ab078df1634a725d
benie <davinci.monalissa@gmail.com>
2022-07-19 11:18:36 +0300

    create a urlconf for preorder

--- tam/urls.py
+++ tam/urls.py
@@ -17,5 +17,6 @@ urlpatterns = [
 
     path('',views.home,name='home'),
     path('menu/<menu_id>',views.menu,name='menu'),
-    path('menus/search-by-ingredient-results',views.search_by_ingredient,name='search-results')
+    path('menus/search-by-ingredient-results',views.search_by_ingredient,name='search-results'),
+    path('menu/pre-order/<menu_id>',views.preorder,name='preorder'),
 ]
