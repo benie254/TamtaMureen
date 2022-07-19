07298b679bc94146afcbbf9f47b434b9f93c4812
benie <davinci.monalissa@gmail.com>
2022-07-19 09:07:56 +0300

    create a urlconf for a single menu

--- tam/urls.py
+++ tam/urls.py
@@ -7,12 +7,14 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin', admin.site.urls,name='admin'),
+
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('django_registration.backends.one_step.urls')),
-    
     path('accounts/signin', auth_views.LoginView.as_view(template_name='registration/login.html'),name='signin'),
     path('accounts/signout', auth_views.LogoutView.as_view(template_name='registration/logout.html'),name='signout'),
     path('accounts/profile',auth_views.LoginView.as_view(template_name='registration/login_success.html'),name='signin-success'),
     path('user/profile/<user_id>/',views.profile,name='profile'),
+
     path('',views.home,name='home'),
+    path('menu/<menu_id>',views.menu,name='menu')
 ]
