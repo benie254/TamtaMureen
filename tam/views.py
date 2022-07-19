be8aec05ab46decef2bf84bb3e1499925c67d074
benie <davinci.monalissa@gmail.com>
2022-07-19 09:20:11 +0300

    create a date today variable

--- tam/views.py
+++ tam/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from tam.models import Menu, Profile 
+import datetime as dt
 
 
 # Create your views here.
@@ -14,12 +15,13 @@ def profile(request,user_id):
     return render(request,'user/profile.html',{"profile":profile,"details":details,"message":message})
 
 def home(request):
+    date_today = dt.date.today()
     user = request.user
     username = user.username 
     message = 'Welcome, ' + username 
 
     menus = Menu.objects.all()
-    return render(request,'content/index.html',{"message":message,"menus":menus})
+    return render(request,'content/index.html',{"message":message,"menus":menus,"date_today":date_today})
 
 def menu(request,menu_id):
     menu = Menu.objects.all().filter(pk=menu_id)
