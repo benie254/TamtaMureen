e854285ca2f2e04f835c47578cdddc7692726000
benie <davinci.monalissa@gmail.com>
2022-07-19 11:32:28 +0300

    add date widget

--- tam/forms.py
+++ tam/forms.py
@@ -3,4 +3,10 @@ from django import forms
 
 class PreorderForm(forms.Form):
     name = forms.CharField(max_length=60)
-    date = forms.DateField()
+    date = forms.DateTimeField(
+        input_formats=['%d/%m/%Y %H:%M'],
+        widget=forms.DateTimeInput(attrs={
+            'class': 'form-control datetimepicker-input',
+            'data-target': '#datetimepicker1'
+        })
+    )
