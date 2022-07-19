7d01167889296e67e1952d18be5723c8a4226c77
benie <davinci.monalissa@gmail.com>
2022-07-19 11:35:12 +0300

    change date time field to date field && update widget

--- tam/forms.py
+++ tam/forms.py
@@ -3,10 +3,6 @@ from django import forms
 
 class PreorderForm(forms.Form):
     name = forms.CharField(max_length=60)
-    date = forms.DateTimeField(
-        input_formats=['%d/%m/%Y %H:%M'],
-        widget=forms.DateTimeInput(attrs={
-            'class': 'form-control datetimepicker-input',
-            'data-target': '#datetimepicker1'
-        })
+    date = forms.DateField(
+        widget=forms.SelectDateWidget
     )
