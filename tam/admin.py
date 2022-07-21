2ab69f669db72a90a2910d47109f23b71e689c6c
benie <davinci.monalissa@gmail.com>
2022-07-21 07:08:31 +0300

    deregister profile model in the admin & register super model

--- tam/admin.py
+++ tam/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from tam.models import Profile,ingredient,Menu 
+from tam.models import Super,ingredient,Menu 
 
 # Register your models here.
 class MenuAdmin(admin.ModelAdmin):
     filter_horizontal=('ingredients',)
 
 
-admin.site.register(Profile)
+admin.site.register(Super)
 admin.site.register(ingredient)
 admin.site.register(Menu,MenuAdmin)
